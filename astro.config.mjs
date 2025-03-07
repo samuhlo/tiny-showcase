afb7487021d87d4f66e78097aa61152e56d2cac5
samU13 <samu13lop@gmail.com>
2025-03-07 13:41:27 +0100

    fix cloudflare astro site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
 
 import react from "@astrojs/react";
 
@@ -9,6 +10,8 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: cloudflare(),
   integrations: [
     react(),
     icon(),
